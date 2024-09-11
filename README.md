1ba6d39f44b1d635f6d182b3592eda2bfdcfd310
rahulrameshwarsharma <rahul.rameshwar.sharma@gmail.com>
2024-09-11 23:45:17 +0530

    user model added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Setting Up the MERN Booking App
+
+first backend setup, then frontend and then mongoDB setup then .env
+
+0fKWhPXIn29PDUNm
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,19 @@
-import express from "express";
+import express, { Request, Response} from "express";
 import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+app.get("/api/test", async (req: Request, res: Response)=> {
+    res.json({message: "Hello from express endpoint!"});
+});
+
+app.listen(7000, ()=>{
+    console.log("server is running at http://localhost:7000");
+});
